232c620f56550e979abb93eb60e77e0580bf34cf
Maid.in.China <maidin_china@163.com>
2013-08-09 21:59:07 +0800

    rr

--- HeroServer/src/com/wolf/dotah/server/cmpnt/data/CardParser.java
+++ HeroServer/src/com/wolf/dotah/server/cmpnt/data/CardParser.java
@@ -1,6 +1,32 @@
 package com.wolf.dotah.server.cmpnt.data;
 
 
+import java.io.FileReader;
+
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+
+
 public class CardParser {
     
+    private static String path = "doc/cards";
+    
+    
+    public static void main(String... args) throws Exception {
+    
+        System.out.println(getAmount());
+    }
+    
+    
+    public static boolean getAmount() throws Exception {
+    
+        JsonReader jsonReader = new JsonReader(new FileReader(path));
+        JsonParser parser = new JsonParser();
+        return parser.parse(jsonReader).isJsonObject();
+    }
+    
+    
+    public static void getCard() {
+    
+    }
 }
